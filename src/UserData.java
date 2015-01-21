887993d43f10fc3a02e2bcb05b031625b384ea72
AAlenushka <aalenushka2010@gmail.com>
2015-01-21 23:39:46 +0200

    Updated UserData Class

--- src/UserData.java
+++ src/UserData.java
@@ -1,56 +1,81 @@
-import java.awt.EventQueue;
-
-import java.awt.ScrollPane;
-import java.awt.BorderLayout;
-import java.awt.Panel;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JTextPane;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.Icon;
 
 
-public class UserData {
 
-	private JFrame frame;
+/**
+ * Created by aandreyeva on 1/15/2015.
+ */
 
-	public UserData() {
-		initialize();
-	}
+public class UserData extends JFrame {
 
-	public void setVisible(boolean b){//to show frame without making it public
-		frame.setVisible(b);
-	}
+    private JPanel thePanel = new JPanel();
+    private JLabel label1 = new JLabel ("Бронювання створене"); //Reservation #?
+    private JLabel label2 = new JLabel("Час виїзду | Маршрут | Місце | Вартість");
+    private JTextField textField1 = new JTextField(42);//Think how to input reservation details into this field
+    private Icon d = new ImageIcon(getClass().getResource("D.png"));
+    private JButton button1 = new JButton("Скасувати бронювання", d);
+    private JLabel label3 = new JLabel("Email");
+    private JTextField textField2 = new JTextField(21);
+    private Icon s = new ImageIcon(getClass().getResource("S.png"));
+    private JButton button2 = new JButton("Відправити деталі бронювання", s);
 
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 490, 329);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JLabel label = new JLabel("\u0412\u0432\u0435\u0434\u0456\u0442\u044C \u0412\u0430\u0448\u0456 \u0414\u0430\u043D\u0456");
-		label.setFont(new Font("Tahoma", Font.BOLD, 27));
-		label.setBounds(37, 16, 455, 50);
-		frame.getContentPane().add(label);
-		
-		JLabel lblNewLabel = new JLabel("E-mail:");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel.setBounds(53, 95, 85, 50);
-		frame.getContentPane().add(lblNewLabel);
-		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(142, 107, 228, 26);
-		frame.getContentPane().add(textPane);
-		
-		JButton btnNewButton = new JButton("\u041E\u043F\u043B\u0430\u0442\u0438\u0442\u0438");
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 20));
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton.setBounds(142, 181, 228, 38);
-		frame.getContentPane().add(btnNewButton);
-	}
+    //Constructor
+    UserData(){
+
+        this.setSize(650, 450);//window size
+        Toolkit tk = Toolkit.getDefaultToolkit();//creating Toolkit object
+        Dimension dim = tk.getScreenSize();
+        int xPOS = (dim.width / 2) - (this.getWidth() / 2);
+        int yPOS = (dim.height / 2) - (this.getHeight() / 2);
+        this.setLocation(xPOS, yPOS);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Щасливий Квиток: Завершення бронювання квитка");
+        thePanel.setBackground(Color.cyan);
+        thePanel.add(label1);
+        thePanel.add(label2);
+        thePanel.add(textField1);
+        button1.setToolTipText("Натискання на цю кнопку видалить деталі бронювання");
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(20, 20, 20, 20);
+        c.gridx = 0;
+        c.gridy = 1;
+        thePanel.add(button1, c);
+        label3.setToolTipText("Введіть валідну адресу, щоб відправити деталі бронювання по електронній пошті");
+        thePanel.add(label3);
+        thePanel.add(textField2);
+        c.gridx = 0;
+        c.gridy = 2;
+        thePanel.add(button2, c);
+        this.add(thePanel);
+        this.setVisible(true);
+        textField1.requestFocus();
+    }
+
+    //Methods
+    void addDeleteListener(ActionListener listenForDeleteButton){
+        button1.addActionListener(listenForDeleteButton);
+    }
+
+    public int getEmail(){
+        return Integer.parseInt(textField2.getText());
+
+    }
+
+    void addSendListener(ActionListener listenForSendButton){
+        button2.addActionListener(listenForSendButton);
+    }
+
+    void displayErrorMessage(String errorMessage){
+        JOptionPane.showMessageDialog(this,errorMessage);
+    }
+
 }
+
+
